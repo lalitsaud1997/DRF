0108a4c097eb69bf5a51d1a1283e61be9cdb1fde
lalit saud <lalitsaud1997@gmail.com>
2022-05-06 15:55:11 +0545

    update and delete operations

--- day3/api/views.py
+++ day3/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 import io
 from rest_framework.parsers import JSONParser
 from .models import Student
@@ -13,6 +12,7 @@ from django.views.decorators.csrf import csrf_exempt
 # for by pass csrf_token
 @csrf_exempt
 def student_api(request):
+    #for data read.....
     if request.method == 'GET':
         json_data = request.body
         stream = io.BytesIO(json_data)
@@ -31,6 +31,7 @@ def student_api(request):
         json_data=JSONRenderer().render(serializer.data)
         return HttpResponse(json_data, content_type='application/json')
 
+    #For create data.....
     if request.method == 'POST':
         json_data = request.body
         stream = io.BytesIO(json_data)
@@ -46,3 +47,38 @@ def student_api(request):
         # if condition validation is false... then to return error message
         json_data = JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data, content_type='application/json')
+    
+    #For update data.......
+    if request.method == 'PUT':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        stu = Student.objects.get(id=id)
+        #for partial update use partial=True....
+        #for complete update remove partial=True
+        serializer = StudentSerializer(stu, data=python_data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            response = {'msg':'Data Updated !!!!'}
+            json_data = JSONRenderer().render(response)
+            return HttpResponse(json_data, content_type='application/json')
+            #return JsonResponse(response, safe=False)
+
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type = 'application/json')
+        
+
+    #For delete data.........
+    if request.method =='DELETE':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        id = python_data.get('id')
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        response = {'msg':'Data Deleted !!!!'}
+        # json_data = JSONRenderer().render(response)
+        # return HttpResponse(json_data, content_type='application/json')
+        return JsonResponse(response, safe=False)
+    
--- day3/myapp.py
+++ day3/myapp.py
@@ -3,6 +3,7 @@ import requests
 import json
 URL = "http://127.0.0.1:8000/studentapi/"
 
+#For Data Read:
 def get_data(id = None):
     data = {}
     if id is not None:
@@ -13,15 +14,41 @@ def get_data(id = None):
     print(data)
 #get_data(1)
 
+#For Data Create:
 def post_data():
     data = {
-        'name':'Hari',
-        'roll':601,
-        'city':'Birgunge'
+        'name':'Bijay Khadka',
+        'roll':2,
+        'city':'Suncity, Kathmandu'
     }
     json_data = json.dumps(data)
     response = requests.post(url=URL, data=json_data)
-
     data = response.json()
     print(data)
-post_data()
+#post_data()
+
+#For Update Data....(partial=True in serializer data for partial update),
+#  and(partial=True remove in serializer data for complete data)
+#and using PUT method....
+def update_data():
+    data = {
+        'id':4,
+        'name':'Bijay kc',
+        'roll':300,
+    }
+    json_data = json.dumps(data)
+    response = requests.put(url=URL, data=json_data)
+    data = response.json()
+    print(data)
+update_data()
+
+# For Delete data.....
+def delete_data():
+    data = {
+        'id':1
+    }
+    json_data = json.dumps(data)
+    response = requests.delete(url=URL, data=json_data)
+    data = response.json()
+    print(data)
+#delete_data()
