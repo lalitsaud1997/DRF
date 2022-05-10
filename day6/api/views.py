0b1b031c4c0c759e49dd3b28c0ee0300efa1166f
lalit saud <lalitsaud1997@gmail.com>
2022-05-10 14:52:50 +0545

    CRUD operation on function based views

---
+++ day6/api/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+
+# Create your views here.
+@api_view(['GET','POST','PUT','PATCH','DELETE'])
+def student_api(request, pk=None):
+    if request.method == 'GET':
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
