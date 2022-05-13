785a21711e7bd7c846c73d9273538def58ebde4e
lalit saud <lalitsaud1997@gmail.com>
2022-05-13 09:22:40 +0545

    concrete view class minimize code

--- day10/api/views.py
+++ day10/api/views.py
@@ -1,34 +1,57 @@
-#GenericAPIView and Model Mixin
+# from .models import Student
+# from .serializers import StudentSerializer
+# from rest_framework.generics import ListAPIView, CreateAPIView,RetrieveAPIView, UpdateAPIView, DestroyAPIView, ListCreateAPIView, RetrieveUpdateAPIView,RetrieveDestroyAPIView,RetrieveUpdateDestroyAPIView
+
+
+# class StudentList(ListAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+
+# class StudentCreate(CreateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentRetrieve(RetrieveAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentUpdate(UpdateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentDelete(DestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+
+# class StudentListCreate(ListCreateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentRetrieveUpdate(RetrieveUpdateAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentRetrieveDelete(RetrieveDestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+# class StudentRetrieveUpdateDelete(RetrieveUpdateDestroyAPIView):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+
+
+#grouping......shortcut.....
+
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-
-
-# List and Create - pk not required
-class List_create_StudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+class StudentListCreate(ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-#Retrive, Update and Delete - pk required
-class Retrive_Update_Delete_StudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+class StudentRetrieveUpdateDelete(RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-        
--- day10/day10/urls.py
+++ day10/day10/urls.py
@@ -4,8 +4,16 @@ from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/', views.List_create_StudentAPI.as_view()),
-    path('studentapi/<int:pk>/', views.Retrive_Update_Delete_StudentAPI.as_view()),
-    
-    
+    # #path('studentapi/', views.StudentList.as_view()),
+    # #path('studentapi/', views.StudentCreate.as_view()),
+    # #path('studentapi/<int:pk>/', views.StudentRetrieve.as_view()),
+    # #path('studentapi/<int:pk>/', views.StudentUpdate.as_view()),
+    # # path('studentapi/<int:pk>/', views.StudentDelete.as_view()), 
+    # path('studentapi/', views.StudentListCreate.as_view()),
+    # #path('studentapi/<int:pk>/', views.StudentRetrieveUpdate.as_view()),
+    # #path('studentapi/<int:pk>/', views.StudentRetrieveDelete.as_view()),
+    # path('studentapi/<int:pk>/', views.StudentRetrieveUpdateDelete.as_view()),
+
+    path('studentapi/', views.StudentListCreate.as_view()),
+    path('studentapi/<int:pk>/', views.StudentRetrieveUpdateDelete.as_view()),
 ]
