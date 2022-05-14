232ca35ba78da83fc369f6d62cb8e9f4d4dc7c7b
lalit saud <lalitsaud1997@gmail.com>
2022-05-14 10:25:33 +0545

    Token Authentication

---
+++ day17/api/models.py
@@ -0,0 +1,28 @@
+#This signal creates auth token for user
+#for signals authtoken.... or you can create new user also create that usertoken at same time...
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+
+
+
+
+#models.......
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    roll = models.IntegerField()
+    city = models.CharField(max_length=100)
+
+
+
---
+++ day17/api/views.py
@@ -0,0 +1,25 @@
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import viewsets
+
+# #for custompermission authentication
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # authentication_classes = [SessionAuthentication]
+    # permission_classes = [IsAuthenticated]
+
+
+
+
+
+
+#using signals for authtoken...
+
+
